107a24937cdc7c9ce28c9c141fc2397fd55d3d0a
wFanhua <744118724@qq.com>
2019-11-01 22:10:38 +0800

    feat(jwt): 处理jwt verify过程中可能产生的错误

--- src/constants/jwt.js
+++ src/constants/jwt.js
@@ -4,7 +4,13 @@
  */
 
 const JWT_AUTH_C_M = [20003, '无访问权限'];
+const JWT_EXPIRED_C_M = [20004, 'token已过期'];
+const JWT_FAIL_C_M = [20005, 'jwt验证失败'];
+const JWT_NOT_BEFORE_C_M = [20006, 'jwt未激活'];
 
 module.exports = {
   JWT_AUTH_C_M,
+  JWT_EXPIRED_C_M,
+  JWT_FAIL_C_M,
+  JWT_NOT_BEFORE_C_M,
 };
--- src/middlewares/jwt/error.js
+++ src/middlewares/jwt/error.js
@@ -1,9 +1,27 @@
-const { JWT_AUTH_C_M } = require('../../constants/jwt');
+const {
+  JsonWebTokenError,
+  NotBeforeError,
+  TokenExpiredError,
+} = require('jsonwebtoken');
+const {
+  JWT_AUTH_C_M,
+  JWT_EXPIRED_C_M,
+  JWT_FAIL_C_M,
+  JWT_NOT_BEFORE_C_M,
+} = require('../../constants/jwt');
 
 module.exports = async (ctx, next) => {
   if (ctx.state && ctx.state.jwtOriginalError) {
-    const errorRes = ctx.fail(...JWT_AUTH_C_M);
-    ctx.throw(401, ctx.state.jwtOriginalError.message, { errorRes });
+    let errorRes;
+    const originError = ctx.state.jwtOriginalError;
+    if (originError instanceof JsonWebTokenError) {
+      errorRes = ctx.fail(...JWT_EXPIRED_C_M);
+    } else if (originError instanceof NotBeforeError) {
+      errorRes = ctx.fail(...JWT_NOT_BEFORE_C_M);
+    } else if (originError instanceof TokenExpiredError) {
+      errorRes = ctx.fail(...JWT_FAIL_C_M);
+    } else errorRes = ctx.fail(...JWT_AUTH_C_M);
+    ctx.throw(401, originError.message, { errorRes });
   }
   await next();
 };
